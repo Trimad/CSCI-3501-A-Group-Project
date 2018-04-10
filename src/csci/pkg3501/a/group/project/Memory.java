756b32df90b19ff7053c4029574439e0f5706fba
DonaldWapenski <dwapensk@my.hpu.edu>
2018-04-10 11:36:07 -1000

    Update Memory.java

--- src/csci/pkg3501/a/group/project/Memory.java
+++ src/csci/pkg3501/a/group/project/Memory.java
@@ -4,29 +4,25 @@ public class Memory {
 
     private int cap; //capacity
     private int[] cell;
-    private Memory memory;
+    
 
     //constructor
     public Memory(int _cap) {
         cell = new int[cap];
     }
 
-    //stub
     public int read(int _address) {
-        return memory.cell[_address];
+        return cell[_address];
     }
 
-    //stub
-    public void setMemory(Memory _memory) {
-        memory = _memory;
-    }
+//    public void setMemory(Memory _memory) {
+//        memory = _memory;
+//    }
 
-    //stub
     public void write(int _address, int _data) {
-        memory.cell[_address] = _data;
+        cell[_address] = _data;
     }
 
-    //stub
     public void dump() {
         
         for (int i = 0; i < cell.length; i++) {
