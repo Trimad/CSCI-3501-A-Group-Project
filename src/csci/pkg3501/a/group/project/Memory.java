20697cb8cef883c9cbb85626835c8b814fddf21a
DonaldWapenski <dwapensk@my.hpu.edu>
2018-04-10 12:12:51 -1000

    Update Memory.java

--- src/csci/pkg3501/a/group/project/Memory.java
+++ src/csci/pkg3501/a/group/project/Memory.java
@@ -2,33 +2,35 @@ package csci.pkg3501.a.group.project;
 
 public class Memory {
 
-    private int cap; //capacity
-    private int[] cell;
-    
+    int cap; //capacity
+    int[] cell; //Memory contains an array of integer cells
 
     //constructor
     public Memory(int _cap) {
-        cell = new int[cap];
+
+        cell = new int[_cap];
+    }
+    
+    public Memory() {
+        this(256);
     }
 
     public int read(int _address) {
         return cell[_address];
     }
 
-//    public void setMemory(Memory _memory) {
-//        memory = _memory;
-//    }
+    public void setMemory() {
+    }
 
     public void write(int _address, int _data) {
         cell[_address] = _data;
     }
 
     public void dump() {
-        
+
         for (int i = 0; i < cell.length; i++) {
-            cell[i] = -1; //-1 is how I'm defining an empty cell
+            System.out.println(cell[i]);
         }
-        
-        System.out.println("\033[32m" +"Memory dump complete.");
+
     }
 }
