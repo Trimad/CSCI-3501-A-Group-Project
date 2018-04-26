ba041e22c8a486ab64b29c1936b45a61eaa8664f
DonaldWapenski <dwapensk@my.hpu.edu>
2018-04-26 11:21:57 -1000

    registers and memory buttons work

--- src/csci/pkg3501/a/group/project/Micro1Viewer.java
+++ src/csci/pkg3501/a/group/project/Micro1Viewer.java
@@ -4,41 +4,39 @@ import javax.swing.*;       //needed for swing classes
 import java.awt.*;
 import java.awt.event.*;    //for actionListener interface
 
-public class Micro1Viewer extends JFrame {
-
+public class Micro1Viewer extends JFrame
+{
     private RegLabelPanel registers;
     private RegValuesPanel values;
     private ButtonsPanel buttons;
     private StepPanel step;
-
-    Console console;
-
-    public Micro1Viewer(Console _console) {
-        this.console = _console;
+    
+    public Micro1Viewer(Console console) 
+    {
         //set window title
         setTitle("Micro1Viewer");
-
+        
         //close window with 'x' at window's right, top corner
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        
         //create panel objects
-        registers = new RegLabelPanel(console);
+        registers = new RegLabelPanel();
         values = new RegValuesPanel(console);
-        buttons = new ButtonsPanel(console);
+        buttons = new ButtonsPanel(console, values);
         step = new StepPanel(console);
-
+        
         //set BorderLayout manager
         setLayout(new BorderLayout());
-
+        
         //add panels
         add(registers, BorderLayout.WEST);
         add(values, BorderLayout.CENTER);
         add(step, BorderLayout.EAST);
         add(buttons, BorderLayout.SOUTH);
-
+        
         //display window
         pack();
         setVisible(true);
     }
-
+    
 }
