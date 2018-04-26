75b9078e704eb04db75c3c15795deb11e668dbb4
DonaldWapenski <dwapensk@my.hpu.edu>
2018-04-26 11:28:45 -1000

    memory and registers buttons work

--- src/csci/pkg3501/a/group/project/RegLabelPanel.java
+++ src/csci/pkg3501/a/group/project/RegLabelPanel.java
@@ -1,10 +1,11 @@
+
 package csci.pkg3501.a.group.project;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class RegLabelPanel extends JPanel {
-
+public class RegLabelPanel extends JPanel
+{
     //labels for regLabelPanel
     private JLabel R0Label;
     private JLabel R1Label;
@@ -17,43 +18,43 @@ public class RegLabelPanel extends JPanel {
     private JLabel spaceLabel;
     private JLabel PCLabel;
     private JLabel IRLabel;
-
-    Console console;
-
-    public RegLabelPanel(Console _console) {
-        this.console = _console;
-        //create a grid layout manager,
-        setLayout(new GridLayout(12, 1));
-
-        //create labels
-        R0Label = new JLabel("        R0         ");
-        R1Label = new JLabel("        R1         ");
-        R2Label = new JLabel("        R2         ");
-        R3Label = new JLabel("        R3         ");
-        R4Label = new JLabel("        R4         ");
-        R5Label = new JLabel("        R5         ");
-        R6Label = new JLabel("        R6         ");
-        R7Label = new JLabel("        R7         ");
-        spaceLabel = new JLabel(" ");
-        PCLabel = new JLabel("        PC         ");
-        IRLabel = new JLabel("        IR         ");
-
-        //add title border to regLabelBorder
-        setBorder(BorderFactory.createTitledBorder("Register"));
-
-        //add components to regLabelPanel
-        add(R0Label);
-        add(R1Label);
-        add(R2Label);
-        add(R3Label);
-        add(R4Label);
-        add(R5Label);
-        add(R6Label);
-        add(R7Label);
-        add(spaceLabel);
-        add(PCLabel);
-        add(IRLabel);
-
-    }
-
+    private JLabel fNameLabel;
+    
+    public RegLabelPanel ()
+        {
+            //create a grid layout manager,
+            setLayout(new GridLayout(13,1));
+            
+            //create labels
+            R0Label = new JLabel("        R0         ");
+            R1Label = new JLabel("        R1         ");
+            R2Label = new JLabel("        R2         ");
+            R3Label = new JLabel("        R3         ");
+            R4Label = new JLabel("        R4         ");
+            R5Label = new JLabel("        R5         ");
+            R6Label = new JLabel("        R6         ");
+            R7Label = new JLabel("        R7         ");
+            spaceLabel = new JLabel(" ");
+            PCLabel = new JLabel("        PC         ");
+            IRLabel = new JLabel("        IR         ");
+            fNameLabel = new JLabel("  File Name");
+                        
+            //add title border to regLabelBorder
+            setBorder(BorderFactory.createTitledBorder("Register"));
+            
+            //add components to regLabelPanel
+            add(R0Label);
+            add(R1Label);
+            add(R2Label);
+            add(R3Label);
+            add(R4Label);
+            add(R5Label);
+            add(R6Label);
+            add(R7Label);
+            add(spaceLabel);
+            add(PCLabel);
+            add(IRLabel);
+            add(fNameLabel);
+        }
+    
 }
