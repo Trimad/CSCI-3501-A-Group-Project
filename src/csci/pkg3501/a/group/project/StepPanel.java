e25adec235591dfca2ecf102df1e96401ad0c3d7
DonaldWapenski <dwapensk@my.hpu.edu>
2018-04-26 11:43:45 -1000

    step button works

--- src/csci/pkg3501/a/group/project/StepPanel.java
+++ src/csci/pkg3501/a/group/project/StepPanel.java
@@ -1,54 +1,55 @@
 package csci.pkg3501.a.group.project;
 
-import javax.swing.*;
+import javax.swing.*;       
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.*;    
 
-public class StepPanel extends JPanel {
-
+public class StepPanel extends JPanel
+{
     private JLabel stepInstructions;
     private JTextField stepValue;
     private JButton stepButton;
-
-    Console console;
-
-    public StepPanel(Console _console) {
-        this.console = _console;
+    private Console console;
+    
+    public StepPanel(Console console) 
+    {
+        this.console = console;
         //create a grid layout manager,
-        setLayout(new GridLayout(5, 1));
-
+        setLayout(new GridLayout(5,1));
+            
         //create label, textfield, button
-        stepInstructions = new JLabel("Enter register");
+        stepInstructions = new JLabel("Enter number");
         stepValue = new JTextField(8);
-
+        
         stepButton = new JButton("Step");
         stepButton.addActionListener(new StepButtonListener());
-
+        
         setBorder(BorderFactory.createTitledBorder("Step"));
-
+      
         //add textfield and button to panel
         add(stepInstructions);
         add(stepValue);
         add(stepButton);
     }
-
-    private class StepButtonListener implements ActionListener {
-
-        public void actionPerformed(ActionEvent e) {
-
+        
+    private class StepButtonListener implements ActionListener 
+    {
+        public void actionPerformed(ActionEvent e) 
+        {
+            
             String input; //user input
-            boolean result;
-
+            boolean result;   
+            
             //get input
             input = stepValue.getText();
             int value = Integer.parseInt(input);
-
-            //step
-            //result = Processor.step();
-            //Display the result
-            JOptionPane.showMessageDialog(null, input);
             
-        }
+            if (value > 0) {
+                console.step(value);
+            } else {
+                System.out.println("Please enter a positive integer.");
+            }
+        }    
     }
-
+    
 }
